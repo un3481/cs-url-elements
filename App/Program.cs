7bfe9760a698a90d07f8abb76436e60691f14380
rendezvous <78743473+un3481@users.noreply.github.com>
2022-10-07 21:29:04 -0300

    Update Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -15,13 +15,21 @@ namespace App
             var url = args[0];
             try
             {
+                // Download Browser
                 await new BrowserFetcher().DownloadAsync();
                 var browser = await Puppeteer.LaunchAsync(GetOptions());
+                // Get Page Content
                 var page = await browser.NewPageAsync();
                 await page.GoToAsync(url);
-                await page.ScreenshotAsync("print.jpg");
-
-                Console.WriteLine(url);
+                // Wait until no more resources are loaded
+                await page.WaitForTimeoutAsync(3000);
+                // Get All Elements
+                var content = await page.GetContentAsync();
+                // Close Browser
+                await browser.CloseAsync();
+                // Write to File
+                var path = DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".html";
+                File.WriteAllText(path, content);
             }
             catch (Exception ex)
             {
@@ -33,18 +41,45 @@ namespace App
         {
             return new LaunchOptions
             {
+                ExecutablePath = "/usr/bin/chromium-browser",
                 Headless = false,
                 Timeout = 0,
-                Args = new string[] {
-                    "--no-sandbox",
-                    "--disable-notifications",
-                    "--disable-dev-shm-usage",
-                    "--disable-setuid-sandbox",
-                    "--lang=en-US,en-GB,en",
-                },
+                LogProcess = false,
                 Devtools = false,
                 IgnoreHTTPSErrors = true,
-                DumpIO = false
+                DumpIO = false,
+                Args = new []
+                {
+                    "--disable-web-security",
+                    "--disable-background-networking",
+                    "--disable-background-timer-throttling",
+                    "--disable-backgrounding-occluded-windows",
+                    "--disable-breakpad",
+                    "--disable-client-side-phishing-detection",
+                    "--disable-default-apps",
+                    "--disable-dev-shm-usage",
+                    "--disable-extensions",
+                    "--disable-features=site-per-process",
+                    "--disable-hang-monitor",
+                    "--disable-ipc-flooding-protection",
+                    "--disable-popup-blocking",
+                    "--disable-prompt-on-repost",
+                    "--disable-renderer-backgrounding",
+                    "--disable-sync",
+                    "--disable-translate",
+                    "--metrics-recording-only",
+                    "--safebrowsing-disable-auto-update",
+                    "--mute-audio",
+                    "--disable-gpu",
+                    "--disk-cache-dir=\"C:\\ProgramData\\PuppeteerService\\Cache\"",
+                    "--enable-accelerated-mjpeg-decode",
+                    "--enable-accelerated-video",
+                    "--enable-gpu-rasterization",
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--enable-native-gpu-memory-buffers",
+                    "--ignore-gpu-blacklist"
+                }
             };
         }
     }
